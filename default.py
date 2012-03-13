2e21ce49fccc61d19ed8675b389dcfb028bf79f8
Daniel Söbirk <daniel_sobirk@yahoo.com>
2012-03-13 12:26:14 +0100

    Reorder categories. Get all clips through some magic

--- default.py
+++ default.py
@@ -8,8 +8,13 @@ import xbmcaddon
 
 from xml.dom.minidom import parse, parseString
 
+import base64
+
 CATEGORY_URL = "http://www.expressen.se/Handlers/WebTvHandler.ashx?id=8793&output=MenuOutput"
 HANDLER_URL = "http://www.expressen.se/Handlers/WebTvHandler.ashx?id="
+
+MAGIC_URLS = {"8877":"3644404&tab=1", "8878":"3644404&tab=0", "8769":"3644404&tab=2", "8773":"3644404&tab=3", "8794":"3643234&tab=0", "8927":"3643225&tab=0", "8930":"3643242&tab=0", "8826":"3644741&tab=0", "8831":"3643244&tab=0", "3646739":"3643244&tab=1", "8886":"3643350&tab=0", "8792":"3643267&tab=1", "8785":"3646139&tab=0", "8770":"3643354&tab=0", "8816":"3646457&tab=0", "8791":"3646457&tab=1", "8776":"3643288&tab=1", "8775":"3643288&tab=2", "8870":"3643288&tab=3", "8887":"3643334&tab=0", "8884":"3643334&tab=1", "8867":"3643323&tab=0", "8852":"3643337&tab=1", "8801":"3643333&tab=0", "8766":"3643338&tab=0", "8767":"3643341&tab=0", "8765":"3643347&tab=0", "3680616":"3688306&tab=0", "8778":"3643342&tab=0", "8782":"3643339&tab=0", "8768":"3643339&tab=1"} 
+
 
 __settings__ = xbmcaddon.Addon(id='plugin.video.expressentv')
 
@@ -21,12 +26,6 @@ def list_categories():
             title = get_node_value(category, "title").encode('utf_8')
             categoryid = get_node_value(category, "id").encode('utf_8')
             add_posts(title, MODE + "category/" + categoryid, isFolder=True)
-            subcategories = category.getElementsByTagName("subcategories")
-            if subcategories != None and len(subcategories) > 0: 
-                for subcategory in subcategories[0].getElementsByTagName("subcategory"):
-                    title = get_node_value(subcategory, "title").encode('utf_8')
-                    categoryid = get_node_value(subcategory, "id").encode('utf_8')
-                    add_posts(title, MODE + "category/" + categoryid, isFolder=True)
     else:
         if state == "site":
             xbmc.executebuiltin('Notification("Expressen TV","Site down")')
@@ -34,10 +33,53 @@ def list_categories():
             xbmc.executebuiltin('Notification("Expressen TV","Malformed result")')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def list_category(categoryid, extraparam = ''):
-    categoryurl = HANDLER_URL + categoryid + extraparam
-    print "url: " + categoryurl
-    doc, state = load_xml(categoryurl)
+def list_subcategories(cid):
+    doc, state = load_xml(CATEGORY_URL)
+    if doc and not state:
+        add_posts("Sök", MODE + "search/", isFolder=True)
+        for category in doc.getElementsByTagName("category"):
+            categoryid = get_node_value(category, "id").encode('utf_8')
+            if categoryid == cid:
+                title = get_node_value(category, "title").encode('utf_8')
+                add_posts(title, MODE + "items/" + categoryid, isFolder=True)
+                subcategories = category.getElementsByTagName("subcategories")
+                if subcategories != None and len(subcategories) > 0: 
+                    for subcategory in subcategories[0].getElementsByTagName("subcategory"):
+                        title = get_node_value(subcategory, "title").encode('utf_8')
+                        categoryid = get_node_value(subcategory, "id").encode('utf_8')
+                        add_posts(title, MODE + "items/" + categoryid, isFolder=True)
+    else:
+        if state == "site":
+            xbmc.executebuiltin('Notification("Expressen TV","Site down")')
+        else:
+            xbmc.executebuiltin('Notification("Expressen TV","Malformed result")')
+    xbmcplugin.endOfDirectory(HANDLE)
+
+def list_menu():
+    for magic in MAGIC_NUMBERS:
+        url = HANDLER_URL + magic
+        doc, state = load_xml(url)
+        if doc and not state:
+            for item in doc.getElementsByTagName("item"):
+                title = get_node_value(item, "title").encode('utf_8')
+                xmlurl = get_node_value(item, "xmlurl").encode('utf_8')
+                add_posts(title, MODE + "list/" + base64.b64encode(xmlurl,"-_"), isFolder=True)
+        else:
+            if state == "site":
+                xbmc.executebuiltin('Notification("Expressen TV","Site down")')
+            else:
+                xbmc.executebuiltin('Notification("Expressen TV","Malformed result")')
+    xbmcplugin.endOfDirectory(HANDLE)
+
+def list_items(categoryid, extraparam = ''):
+    if categoryid in MAGIC_URLS:
+        categoryurl = HANDLER_URL + MAGIC_URLS[categoryid]
+    else:
+        categoryurl = HANDLER_URL + categoryid + extraparam
+    categoryworker(categoryurl)
+
+def categoryworker(url):
+    doc, state = load_xml(url)
     if doc and not state:
         items = doc.getElementsByTagName("items")
         for item in items[0].getElementsByTagName("item"):
@@ -80,9 +122,6 @@ def play_clip(clipid):
                 url = vurl.childNodes[0].data
                 maxbitrate = bitrate
         parts = url.split("/")
-        print "playurl: " + url
-        for p in parts:
-            print p
         rtmpUrl = parts[0] + "/" + parts[1] + "/" + parts[2] + "/" + parts[3]
         playPath = ""
         for i in range(4, len(parts)):
@@ -122,8 +161,7 @@ def do_search():
     keyb.doModal()
     if (keyb.isConfirmed()):
         search = keyb.getText()
-        print "Searching for: " + search
-        list_category("8773", "&search=" + search)
+        list_items("8773", "&search=" + search)
 
 
 def get_node_value(parent, name, ns=""):
@@ -159,23 +197,18 @@ if (__name__ == "__main__" ):
     MODE=sys.argv[0]
     HANDLE=int(sys.argv[1])
     modes = MODE.split('/')
-    print "MODE: " + MODE
-    print "modes done: " + str(len(modes))
-    
-    print "0:" + modes[0]
-    print "1:" + modes[1]
-    print "2:" + modes[2]
-    print "3:" + modes[3]
-
+    MODE = modes[0]+"/"+modes[1]+"/"+modes[2]+"/"
     activemode = modes[len(modes) - 2]
     itemid = modes[len(modes) - 1]
-    print "ac: " + activemode
-    print "cat: " + itemid
     
     if activemode == "category" :
-        list_category(itemid)
+        list_subcategories(itemid)
     elif activemode == "play":
         play_clip(itemid)
+    elif activemode == "list":
+        categoryworker(base64.b64decode(itemid,"-_"))
+    elif activemode == "items":
+        list_items(itemid)
     elif activemode == "search":
         do_search()
     else :
